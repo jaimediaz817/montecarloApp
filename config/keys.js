f87683947c72ac6a3d478af513a0d8abe3c5ab48
Jaime Diaz <jdsolutions817@gmail.com>
2018-12-23 23:46:50 -0500

    cambio de ambiente BD a preproducción

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     //mongoURI: 'mongodb://jdiaz:montecarloAdmin817@ds135724.mlab.com:35724/montecarloconnector',
-    //mongoURI: 'mongodb://jdiazMontecarlo:Jidg123456789@192.168.243.15:27017/montecarloDevDb',
-    mongoURI: 'mongodb://jdiazMontecarloProd:Jidg123456789@165.227.206.235:27017/montecarloProd',
+    mongoURI: 'mongodb://jdiazMontecarlo:Jidg123456789@192.168.243.15:27017/montecarloDevDb',
+    //mongoURI: 'mongodb://jdiazMontecarloProd:Jidg123456789@165.227.206.235:27017/montecarloProd',
     secretOrKey : 'secret'
 };
